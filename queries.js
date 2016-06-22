b5427cc4daa28c17cc77a21a9ef26465e7424305
anna baker <annakatherinebaker@gmail.com>
2016-06-22 17:27:05 -0500

    initial commit

---
+++ queries.js
@@ -0,0 +1,7 @@
+// 2. db.bios.find({"awards": {$exists:true}}).pretty()
+// 3. db.bios.find({"awards": {$exists:false}}).pretty()
+// 4. db.bios.find({ $or: [{"contribs": "UNIX"}, { "contribs": "OOP"}]})
+// 5. db.bios.find({ "awards.award": "Turing Award" })
+// 6. db.bios.find({ _id: { $gt: 3, $lt: 7 } })
+// 7. db.bios.find({ "awards.year": { $lt: 2000 }})
+// 8. db.bio.find({ $and: [{ "birth": {$exists:true}}, {"death": {$exists:false}}]})
